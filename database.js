b135c049579ff98b000fb38b7de74cbdc71a4b7b
nicolobaldari <nicolobaldari@gmail.com>
2026-08-17 15:30:48 +0200

    Sposta credenziali DB in variabili d'ambiente

--- database.js
+++ database.js
@@ -1,14 +1,16 @@
-/*
- * Questo file stabilisce la connessione al database MySQL utilizzando l'ORM Sequelize.
- * Configura i parametri base di accesso (nome db, utente, password, host) necessari
- * per far comunicare l'applicazione backend con il database locale.
- */
+
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-// Inizializza l'istanza di Sequelize con le credenziali del DB MySQL e disabilita i log delle query
-const sequelize = new Sequelize('museo_cose_perdute', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+    }
+);
 
 module.exports = sequelize;
